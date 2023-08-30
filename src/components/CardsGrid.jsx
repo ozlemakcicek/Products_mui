473a184279a17471ee2072e8e7c5ee05597b5962
ozlem <ozlemakcicek83@gmail.com>
2023-08-30 23:57:50 +0200

    responsiv

--- src/components/CardsGrid.jsx
+++ src/components/CardsGrid.jsx
@@ -1,92 +1,69 @@
 import { Container, Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
-
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-
 import ShareIcon from "@mui/icons-material/Share";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-
 const CardsGrid = () => {
   const [products, setProducts] = useState([]);
-
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => setProducts(data.products));
   }, []);
-
   console.log(products);
-
   return (
     <Container>
       <Typography variant="h3" mt={3} sx={{ color: "salmon" }} align="center">
         {" "}
         NEW PRODUCTS
       </Typography>
-
-      <Grid
-        container
-        mt={4}
-        spacing={2}
-       
-      >
+      <Grid container mt={4} spacing={2}>
         {products.map((a) => {
           return (
             <Grid item xs={12} sm={6} md={4}>
               <Card
                 sx={{
-                  width: 370,
-                  height: 500,
-                
-                 
+                  maxWidth: 450,
+                  height: 550,
+                  display: "flex",
+                  justifyContent: "space-between",
+                  flexDirection: "column",
+                  //position:"relative"// ikinci yolu
                 }}
               >
                 <CardMedia
-                  sx={{ height: 200,width:400, objectFit: "contain", py: 2 }}
+                  sx={{ height: 200, maxWidth: 400 }}
                   image={a.images[0]}
                 />
-
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
                     {a.title}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Stock:{a.stock}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="text.secondary"
-                    sx={{
-                      overflow: "hidden", // tasan icerigi gizler
-                      textOverflow: "ellipsis", //tasan metni kirpip (...) ile gosterir
-                      display: "-webkit-box",
-                      WebkitLineClamp: "2",
-                      WebkitBoxOrient: "vertical",
-                    }}
-                  >
                     {a.description}
                   </Typography>
-                  
+                  <Typography variant="body2" color="text.secondary">
+                    Stock:{a.stock}
+                  </Typography>
                 </CardContent>
-
                 <CardActions
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    gap: 2,
-                  }}
+                //sx={{position:"absolute",bottom:0}}//ikinci yol olursa
                 >
                   <Button
                     size="small"
                     variant="contained"
                     color="grey"
-                    sx={{ color: "salmon" }}
+                    sx={{
+                      color: "salmon",
+                      height: 43,
+                  
+                    }} 
+                    
                     startIcon={<ShareIcon sx={{ color: "salmon" }} />}
                   >
                     Share
@@ -95,7 +72,11 @@ const CardsGrid = () => {
                     size="small"
                     variant="contained"
                     color="grey"
-                    sx={{ color: "salmon" }}
+                    sx={{
+                      color: "salmon",
+                      height: 43,
+                      
+                    }}
                     startIcon={<DeleteOutlineIcon sx={{ color: "salmon" }} />}
                   >
                     Delete
@@ -109,5 +90,4 @@ const CardsGrid = () => {
     </Container>
   );
 };
-
 export default CardsGrid;
